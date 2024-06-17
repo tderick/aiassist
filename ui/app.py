f1635d4438c46e2145ffbd65ba3d418c0bd6c634
TEMFACK DERICK <dericktemfack@gmail.com>
2024-06-17 20:27:02 +0200

    Connect chainlit with data retrieval services

--- ui/app.py
+++ ui/app.py
@@ -9,8 +9,6 @@ async def start():
 
 @cl.on_message
 async def main(message: cl.Message):
-    # Your custom logic goes here...
-
     url = 'http://127.0.0.1:8001/api/v1/retriever/'
     headers = {
         'accept': 'application/json',
@@ -21,8 +19,49 @@ async def main(message: cl.Message):
         'bot_id': 'unipi',
     }
 
-    response = requests.post(url, headers=headers, data=data)
-    # Send a response back to the user
-    await cl.Message(
-        content=response.json()['response'],
-    ).send()
+    try:
+        response = requests.post(url, headers=headers, data=data)
+    except:
+        msg = cl.Message(content="")
+        answer = "Internal server error. Please try again later or contact the administrator."
+        for token in answer:
+            await msg.stream_token(token)
+
+        await msg.send()
+        return
+
+    if response.status_code == 200:
+        rs = response.json()
+
+        answer = rs['data']['response']
+        sources = rs['data']['sources']
+
+        text_elements = []
+
+        for index, source in enumerate(sources):
+            source_name = f"source {index+1}"
+            text_elements.append(
+                cl.Text(content=source['text'], name=source_name, display="side")
+            )
+            
+        source_names = [text_el.name for text_el in text_elements]
+
+        if source_names:
+            answer += f"\n\n\nSources: {', '.join(source_names)}"
+        else:
+            answer += "\nNo sources found"
+
+        msg = cl.Message(content="", elements=text_elements)
+        
+
+        for token in answer:
+            await msg.stream_token(token)
+
+        await msg.send()
+    else:
+        msg = cl.Message(content="")
+        answer = "Internal server error. Please try again later or contact the administrator."
+        for token in answer:
+            await msg.stream_token(token)
+
+        await msg.send()
